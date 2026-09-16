1ccd3ec106ed7dc24f834c88bab5e02d07d541d0
unknown <fasmilkp@gmail.com>
2026-09-16 11:40:01 +0530

    Functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+def welcome():
+    print("Welcome To Python")
+
+welcome()
+
+
+def add(a,b):
+    return a+b
+
+
+print(add(10,20))
+
+
+
+def greet(name):
+    print("Welcome ",name)
+
+greet("abitha")
