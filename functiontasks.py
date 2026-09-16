8efa3f4e9c2b593a6e583e9c399348184824417f
unknown <fasmilkp@gmail.com>
2026-09-16 12:15:44 +0530

    tasks

---
+++ functiontasks.py
@@ -0,0 +1,9 @@
+def sqr(a):
+    print(f"The Square of {a} is {a*a}")
+sqr(2)
+
+
+def salary(basic,bonus=1000):
+    print(f"Salry is {basic} and the total with the bonus is {basic+bonus}")
+
+salary(10000)
