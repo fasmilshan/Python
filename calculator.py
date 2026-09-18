acf380154c55f8713b2cb136cc8b5e573bc235ec
unknown <fasmilkp@gmail.com>
2026-09-18 12:31:07 +0530

    Calculator Updated

--- calculator.py
+++ calculator.py
@@ -1,31 +1,40 @@
 while True:
- print("Calculator")
- print("-----------")
- print("1.Addition")
- print("2.Substarction")
- print("3.Division")
- print("4.Multiplication")
- print("5.Exit")
- choice=input("Enter Your Choice : 1/2/3/4/5 =:")
- if choice == "1":
+ print("-----------------------------")
+ print("|      |Calculator|")
+ print("-----------------------------|")
+ print("|1.Addition                  |")
+ print("|2.Substarction              |")
+ print("|3.Division                  |")
+ print("|4.Multiplication            |")
+ print("|5.Exit                      |")
+ print(" ----------------------------")
+ try:
+   choice=int(input("Enter Your Choice : 1/2/3/4/5 =:"))
+ except:
+   print(f"Must Type Number In The Choice Section!")
+ else:
+   if choice == 1:
     n1=float(input("Enetr Your First Number: "))
     n2=float(input("Enter Your Second Number: "))
     print(n1,"+",n2,"=",n1+n2)
- elif choice== "2":
+   elif choice== 2:
     n1=float(input("Enetr Your First Number: "))
     n2=float(input("Enter Your Second Number: "))
     print(n1,"-",n2,"=",n1-n2)
- elif choice== "3":
+   elif choice== 3:
     n1=float(input("Enetr Your First Number: "))
     n2=float(input("Enter Your Second Number: "))
-    print(n1,"%",n2,"=",n1/n2)
- elif choice== "4":
+    try:
+      print(n1,"%",n2,"=",n1/n2)
+    except:
+      print("You Cant Use 0 in Division!")
+   elif choice== 4:
     n1=float(input("Enetr Your First Number: "))
     n2=float(input("Enter Your Second Number: "))
     print(n1,"x",n2,"=",n1*n2)
- elif choice== "5":
-    break
- elif choice == " ":
+   elif choice== 5:
+      break
+   elif choice == " ":
     print("Please Choose Your Choice From(1,2,3,4,5)!")
- else: 
+   else: 
       print("Invalid Option")
