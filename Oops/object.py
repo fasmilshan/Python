8e9199f9530e52c3bb8a17219de48e314de21d88
unknown <fasmilkp@gmail.com>
2026-09-23 12:47:53 +0530

    Oops-concept

---
+++ Oops/object.py
@@ -0,0 +1,13 @@
+class Student:
+    def greet(self,name):
+        print(f"Good Morning {name}")
+
+
+s1=Student()
+s2=Student()
+
+s1.greet("Fasmil")
+s2.greet("Shan")
+
+
+print(" \nCompleted !\n ---------------------------------")
